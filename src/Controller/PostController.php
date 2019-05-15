7b60ffcd5a839ea948a03975e7bd255ad5df8131
Pierre Guillaume <dev@pierreguillau.me>
2019-05-15 11:05:11 +0200

    adding page single

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use mysql_xdevapi\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,4 +26,28 @@ class PostController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/posts/{id}", name="post_single", requirements={"id"="[0-9]+"})
+     */
+    public function single($id) {
+
+        // On va chercher en BDD le post qui correspond à l'ID
+        $post = $this
+            ->getDoctrine()
+            ->getRepository(Post::class)
+            ->find($id);
+
+        // Si le post n'est pas trouvé, on doit gérer cette erreur
+        if (empty($post)) {
+            // rediriger sur une autre page
+            // return $this->redirectToRoute('posts_list');
+            // ou lancer une erreur 404
+            throw $this->createNotFoundException('Post introuvable');
+        }
+
+        // On passe le post trouvé à la vue
+        return $this->render('post/single.html.twig', [
+            'post' => $post
+        ]);
+    }
 }
