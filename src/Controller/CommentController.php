6ec16fbe2c2e8cb90afe8b2540b345b2048e74cb
Pierre Guillaume <dev@pierreguillau.me>
2019-05-21 11:02:00 +0200

    adding Comment creation

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Entity\User;
+use App\Form\CommentType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/posts/{postId}/comment", name="comment_create")
+     */
+    public function create(Request $request, $postId)
+    {
+
+        $manager = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(CommentType::class);
+
+        $form->handleRequest($request);
+
+        // gérer les données recues
+        // valider les données
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // créer un commentaire
+            $comment = $form->getData();
+
+            // on va cherche un user aléatoire
+            $user = $this
+                ->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneRandom();
+
+            $comment->setAuthor($user);
+
+            // associer le commentaire au post
+             $post = $this
+                 ->getDoctrine()
+                 ->getRepository(Post::class)
+                 ->find($postId);
+
+             $comment->setPost($post);
+
+            // enregistrer le commentaire
+            $manager->persist($comment);
+            $manager->flush();
+        }
+
+        // rediriger sur la single
+        return $this->redirectToRoute('post_single', [
+            'id' => $postId
+        ]);
+    }
+}
