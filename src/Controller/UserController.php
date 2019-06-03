a868ab01a5622615b4c79852b59d7d29de1d4f98
Pierre Guillaume <dev@pierreguillau.me>
2019-06-03 12:40:20 +0200

    base code for profile edition

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserProfileType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/users/{id}", name="user_profile")
+     * @Route("/users/{id<\d+>}", name="user_profile")
      */
     public function profile($id)
     {
+        if(intval($id) === $this->getUser()->getId()) {
+            return $this->redirectToRoute('current_user_profile');
+        }
+
         $user = $this
             ->getDoctrine()
             ->getRepository(User::class)
@@ -26,4 +32,33 @@ class UserController extends AbstractController
             'user' => $user,
         ]);
     }
+
+
+    /**
+     * @Route("/users/me", name="current_user_profile")
+     */
+    public function me(Request $req) {
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserProfileType::class, $user);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted()) {
+
+            if ($form->isValid()) {
+                $this->getDoctrine()->getManager()->flush();
+            }
+        }
+
+        return $this->render('user/profile.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+
+
+    }
+
+
 }
---
+++ src/Form/UserProfileType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+//            ->add('firstName')
+//            ->add('lastName')
+//            ->add('birth')
+//            ->add('email')
+//            ->add('password')
+//            ->add('avatar')
+        ->add('submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
